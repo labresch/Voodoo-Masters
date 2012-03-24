d9ea0d7d7e901a999fcbdec02b7d104d604b0baf
Mark Crisp <macr1324@gmail.com>
2012-03-23 21:54:47 -0700

    paperclass

---
+++ CS304/src/Paper.java
@@ -0,0 +1,32 @@
+import java.sql.Date;
+
+
+public class Paper {
+   
+    private static String editor;
+    private static String title;
+    private static Date datePublished;
+    private static int paperID;
+   
+    public Paper(String nm, String tl, Date datep, int pid) {
+        editor = nm;
+        title = tl;
+        datePublished = datep;
+        paperID = pid;
+    }
+   
+    public String editor() {
+        return editor;
+    }
+    public String title() {
+        return title;
+    }
+    public Date datePublished() {
+        return datePublished;
+    }
+    public int paperID() {
+        return paperID;
+    }
+   
+
+}
